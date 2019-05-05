a22c280ddbd362d0555e5c54a98fdfdb8f19a5fb
adam <adam.bickford@gmail.com>
2019-05-05 13:39:35 -0400

    Chain 3 instances for complete round trip request

--- spring-web-app/src/main/java/com/db/reactivedemo/springwebapp/SpringWebAppApplication.java
+++ spring-web-app/src/main/java/com/db/reactivedemo/springwebapp/SpringWebAppApplication.java
@@ -1,17 +1,51 @@
 package com.db.reactivedemo.springwebapp;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class SpringWebAppApplication {
+    @Value("${downstream.service}")
+    private String downstreamService;
+
+    @Value("${service.name}")
+    private String serviceName;
+
+    private final RestTemplate client = new RestTemplate();
 
     public static void main(String[] args) {
         SpringApplication.run(SpringWebAppApplication.class, args);
     }
 
+    @GetMapping("/{delayMillis}")
+    public String delay(@PathVariable long delayMillis) {
+        work(delayMillis);
+        String result = client.getForObject(downstreamService + "/" + delayMillis, String.class);
+        return String.format("hop (%s) -> %s", serviceName, result);
+    }
 
+
+    @GetMapping("/data/{delayMillis}")
+    public String data(@PathVariable long delayMillis) {
+        work(delayMillis);
+        return "Blocking: " + UUID.randomUUID().toString();
+    }
+
+    private void work(long delayMillis) {
+        try {
+            Thread.sleep(delayMillis);
+        } catch (InterruptedException e) {
+            log.error("Thread interrupted", e);
+        }
+    }
 }
