35268171fe2e824b542b8361c48c72290f8cf23a
adam <adam.bickford@gmail.com>
2019-05-05 13:03:18 -0400

    Basic spring webflux skeleton

---
+++ spring-webflux-app/src/test/java/com/db/reactivedemo/springwebfluxapp/SpringWebfluxAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.db.reactivedemo.springwebfluxapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebfluxAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
