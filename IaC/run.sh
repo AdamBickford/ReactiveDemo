f5953f6fe5a3f241540983f46a75cfd9c1d29b9e
adam <adam.bickford@gmail.com>
2022-08-08 18:16:28 -0400

    Basic load generation in a docker container w/envars in place

--- IaC/run.sh
+++ IaC/run.sh
@@ -4,12 +4,12 @@ aws cloudformation create-stack --stack-name mystack \
   --capabilities CAPABILITY_NAMED_IAM
 
 ###
+              sudo docker run --rm -i grafana/k6 run -e THE_IP=${THE_IP} - <load.js | tail -n 18 > /home/ec2-user/results.txt
 
 aws cloudformation update-stack --stack-name mystack \
   --template-body file://foo.template \
   --parameters ParameterKey=S3BucketResource,ParameterValue=arn:aws:s3:::rx-demo/* \
   --capabilities CAPABILITY_NAMED_IAM
-
 
 aws cloudformation delete-stack --stack-name mystack
 
@@ -20,17 +20,16 @@ aws s3 cp s3://rx-demo/foo.template .
 aws s3 cp s3://../spring-web-app/target/spring-web-app-0.0.1-SNAPSHOT.jar
 
 
-----
 
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 aws s3 cp s3://rx-demo/load.js /home/ec2-user
-sudo docker run --rm -i grafana/k6 run -e THE_IP=host.docker.internal - <load.js
+sudo docker run --rm -i grafana/k6 run -e THE_IP=host.docker.internal - <load.js | echo -e "adam was here:\n\n\n\n\n\n\n\n\n $(tail -n 18)" >results.txt
+tail -n 18 tmp.txt >results.txt
 
-
-sudo docker run --rm -i  influxdb=http://localhost:8086/myk6db grafana/k6 run - <~/code/ReactiveDemo/performance/load.js
+sudo docker run --rm -i influxdb=http://localhost:8086/myk6db grafana/k6 run - <~/code/ReactiveDemo/performance/load.js
 
 #git clone 'https://github.com/grafana/k6'
 #cd k6
@@ -42,3 +41,16 @@ sudo docker run --rm -i  influxdb=http://localhost:8086/myk6db grafana/k6 run - 
 #    k6 run /scripts/load.js
 #
 #docker run --rm -v /c/Users/esp_2/code/ReactiveDemo/performance:/scripts:/src -i grafana/k6 run /src/load.js
+
+docker run --rm -i grafana/k6 run -e THE_IP=${THE_IP} - <load.js | tail -n 18 > /home/ec2-user/results.txt
+
+#    // const BASE_URL = 'http://host.docker.internal:8081/request?latencies='; // make sure this is not production
+
+
+curl http://$(cat theip.txt):8080/request?latencies=1000,2000
+
+
+docker run --rm -i grafana/k6 run -e THE_IP=$(cat theip.txt) - <load.js | tail -n 18 > /home/ec2-user/results.txt
+
+
+docker run --rm -i grafana/k6 run -e THE_IP=host.docker.internal - <load.js | tail -n 18 > results.txt
--- performance/load.js
+++ performance/load.js
@@ -2,32 +2,35 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    noConnectionReuse: true,
     stages: [
-        { duration: '10s', target: 10 }, // below normal load
-        { duration: '1m', target: 50 }, // below normal load
-        // { duration: '1m', target: 100 }, // below normal load
-        // { duration: '1m', target: 100 }, // below normal load
-        // { duration: '1m', target: 200 }, // below normal load
-        // { duration: '1m', target: 300 }, // below normal load
-        // { duration: '1m', target: 400 }, // below normal load
-        // { duration: '1m', target: 400 }, // below normal load
-        // { duration: '1m', target: 800 }, // below normal load
-        // { duration: '1m', target: 800 }, // below normal load
-        // { duration: '1m', target: 1200 }, // below normal load
-        // { duration: '1m', target: 1200 }, // below normal load
-        // { duration: '1m', target: 2000 }, // below normal load
-        // { duration: '1m', target: 2000 }, // below normal load
-        { duration: '1m', target: 0 }, // scale down. Recovery stage.
+        { duration: '10s', target: 10 },
+        { duration: '1m', target: 50 },
+        { duration: '1m', target: 100 },
+        { duration: '1m', target: 100 },
+        { duration: '1m', target: 200 },
+        { duration: '1m', target: 300 },
+        { duration: '1m', target: 400 },
+        { duration: '1m', target: 400 },
+        { duration: '1m', target: 800 },
+        { duration: '1m', target: 800 },
+        { duration: '1m', target: 1200 },
+        { duration: '1m', target: 1200 },
+        // { duration: '1m', target: 1600 },
+        // { duration: '1m', target: 1600 },
+        // { duration: '1m', target: 2000 },
+        // { duration: '1m', target: 2000 },
+        { duration: '30s', target: 0 },
     ],
+    thresholds: {
+        http_req_duration: ['p(90) < 20000', 'p(95) < 40000', 'p(99.9) < 300000'],
+        http_req_failed: ['rate<0.1']
+    }
 };
 
 export default function () {
-    const BASE_URL = `http://${__ENV.THE_IP}:8081/request?latencies=`; // make sure this is not production
-    // const BASE_URL = 'http://localhost:8081/request?latencies='; // make sure this is not production
-    // const BASE_URL = 'http://host.docker.internal:8081/request?latencies='; // make sure this is not production
+    const BASE_URL = `http://${__ENV.THE_IP}:8080/request?latencies=`; // make sure this is not production
 
-    let response = http.get(`${BASE_URL}0,0`);
+    let response = http.get(`${BASE_URL}250,800`);
 
     if (response.status != 200) {
         console.error('Could not send summary, got status ' + response.status);
@@ -35,5 +38,3 @@ export default function () {
 
     sleep(1);
 }
-
-//docker run --rm -i -v "$PWD:/work" grafana/k6 run - <load.js > adamfooagain.txt 2>&1 &
